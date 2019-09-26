b200d65d86aa9b4ab9f3679d1d75f8dfdfcf9f9f
wangdonglin <50353438@qq.com>
2019-09-26 21:10:40 +0800

    process the .prefab of resources directory

--- src/AssetCleaner.js
+++ src/AssetCleaner.js
@@ -39,13 +39,16 @@ let AssetCleaner = {
         destFile = FileHelper.getFullPath(destFile);
 
         this.lookupAssetDir(sourceFile);
-        let outMap = this.compareAssets();
-        let outStr = this.getSortedResult(outMap, sourceFile);
+        let { noBindMap, noLoadMap } = this.compareAssets();
+        let outStr1 = '未引用文件数量=';
+        outStr1 = this.getSortedResult(outStr1, noBindMap, sourceFile);
+        let outStr2 = '非动态调用(无需放在resources下)文件数量=';
+        outStr2 = this.getSortedResult(outStr2, noLoadMap, sourceFile);
+        let outStr = outStr1 + '\n' + outStr2;
         FileHelper.writeFile(destFile, outStr);
     },
 
-    getSortedResult(outMap, srcDir) {
-        let outStr = '未引用文件数量=';
+    getSortedResult(outStr, outMap, srcDir) {
         if (outMap.size <= 0) {
             return outStr;
         }
@@ -83,45 +86,83 @@ let AssetCleaner = {
 
     // UUID和文件逐个比较，返回结果汇总
     compareAssets() {
-        let outMap = new Map();
+        let noBindMap = new Map();
+        let noLoadMap = new Map();
         
         // 如果源UUID在所有目标资源都未找到，则说明源UUID对应的文件未被引用
         for (let [srcPath, srcData] of this.sourceMap.entries()) {
-            let bFound = false;
-            for (let [destPath, destData] of this.destMap.entries()) {
-                if (srcPath == destPath) {
-                    continue;
-                }
-                if (!!srcData && !!srcData.uuid) {
-                    for (let i = 0, len = srcData.uuid.length; i < len; i++) {
-                        let uuid = srcData.uuid[i];
-                        if (destData.data.indexOf(uuid) >= 0) {
-                            bFound = true;
-                            break; // 源数组只要有一个UUID被引用，即代表源文件被引用了，无需继续查找
-                        }
-                    }
-                }
-                if (!!srcData && !!srcData.name && srcData.name.length > 0) {
-                    if (destData.data.indexOf(srcData.name) >= 0) {
-                        bFound = true;
-                    }
-                }
-                if (bFound) {
-                    break; // 源文件只要被一个目标文件引用，即代表源文件被引用了，无需再和之后的目标文件比较
-                }
+            let isBind = false;
+            let isCodeLoad = false;
+            let bDynamic = (srcPath.indexOf('\\resources\\') >= 0);
+            
+            isBind = this.findAssetByUUID(srcPath, srcData);
+            if (bDynamic) {
+                isCodeLoad = this.findAssetByName(srcPath, srcData);
             }
 
-            if (!bFound) {
-                let files = outMap.get(srcData.type);
+            if (!isBind && !isCodeLoad) {
+                let files = noBindMap.get(srcData.type);
                 if (!files) {
                     files = [];
-                    outMap.set(srcData.type, files);
+                    noBindMap.set(srcData.type, files);
+                }
+                files.push({ path:srcPath, size:srcData.size });
+            } else if (bDynamic && isBind && !isCodeLoad) {
+                let files = noLoadMap.get(srcData.type);
+                if (!files) {
+                    files = [];
+                    noLoadMap.set(srcData.type, files);
                 }
                 files.push({ path:srcPath, size:srcData.size });
             }
         }
 
-        return outMap;
+        return { noBindMap, noLoadMap };
+    },
+
+    // 根据源UUID在目标资源中查找
+    findAssetByUUID(srcPath, srcData) {
+        let bFound = false;
+        if (!srcPath || !srcData) {
+            return bFound;
+        }
+        for (let [destPath, destData] of this.destMap.entries()) {
+            if (srcPath == destPath || ResType.Code === destData.type) {
+                continue;
+            }
+            if (!!srcData && !!srcData.uuid) {  
+                for (let i = 0, len = srcData.uuid.length; i < len; i++) {
+                    let uuid = srcData.uuid[i];
+                    if (destData.data.indexOf(uuid) >= 0) {
+                        bFound = true;
+                        return bFound; // 源UUID数组只要有一个被引用，即代表源文件被引用了，无需继续查找
+                    }
+                }
+            }
+        }
+        return bFound;
+    },
+
+    // 根据源文件名在目标资源中查找
+    // 主要是搜索代码中动态调用的.prefab、.anim
+    findAssetByName(srcPath, srcData) {
+        let bFound = false;
+        if (!srcPath || !srcData) {
+            return bFound;
+        }
+        for (let [destPath, destData] of this.destMap.entries()) {
+            // 目标资源必须是.js代码
+            if (srcPath == destPath || ResType.Code !== destData.type) { 
+                continue;
+            }
+            if (!!srcData && !!srcData.name && srcData.name.length > 0) {
+                if (destData.data.indexOf(srcData.name) >= 0) {
+                    bFound = true;
+                    break;
+                }
+            }
+        }
+        return bFound;
     },
 
     // 递归查找指定目录下所有资源
@@ -136,10 +177,6 @@ let AssetCleaner = {
             let file = files[i];
             let curPath = path.join(srcDir, file);
 
-            // 暂时排除src目录
-            // if (curPath.indexOf('\\src\\') >= 0) {
-            //     continue;
-            // }
             // 如果该文件已处理过则直接跳过
             if (this.handleMap.has(curPath)) {
                 continue;
@@ -158,19 +195,19 @@ let AssetCleaner = {
             switch (pathObj.ext) {
                 case '.js':
                     data = FileHelper.getFileString(curPath);
-                    this.destMap.set(curPath, { data });
+                    this.destMap.set(curPath, { data, type:ResType.Code });
                     break;
 
                 case '.prefab':
                     uuid = this.getFileUUID(curPath, pathObj, ResType.Prefab);
-                    data = { uuid, type:ResType.Prefab, size:stats.size };
+                    data = { uuid, type:ResType.Prefab, size:stats.size, name:'' };
                     if (curPath.indexOf('\\resources\\') >= 0) {
                         data.name = pathObj.name; // resources还需记录文件名
                     }
                     this.sourceMap.set(curPath, data);
                     
                     data = FileHelper.getFileString(curPath);
-                    this.destMap.set(curPath, { data });
+                    this.destMap.set(curPath, { data, type:ResType.Prefab });
                     break;
 
                 case '.anim':
@@ -179,12 +216,12 @@ let AssetCleaner = {
                         this.sourceMap.set(curPath, { uuid, type:ResType.Anim, size:stats.size });
                     }
                     data = FileHelper.getFileString(curPath);
-                    this.destMap.set(curPath, { data });
+                    this.destMap.set(curPath, { data, type:ResType.Anim });
                     break;
 
                 case '.fire':
                     data = FileHelper.getFileString(curPath);
-                    this.destMap.set(curPath, { data });
+                    this.destMap.set(curPath, { data, type:ResType.Fire });
                     break;
                 
                 case '.png':
